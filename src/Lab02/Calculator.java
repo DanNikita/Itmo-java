daa0d946fb8dee6d337c01f5b52502e25212d7a2
DanilovNV <1995dnv@gmail.com>
2024-04-13 00:05:43 +0300

    Amended files for lab 1 and 2

---
+++ src/Lab02/Calculator.java
@@ -0,0 +1,53 @@
+package Lab02;
+
+public class Calculator {
+
+    public double CalcSum (double a, double b)
+    {
+      return a + b;
+    }
+    public long CalcSum (long a, long b)
+    {
+        return a + b;
+    }
+    public int CalcSum (int a, int b)
+    {
+        return a + b;
+    }
+    public double CalcDiv (double a, double b)
+    {
+        return a / b;
+    }
+    public double CalcDiv (long a, long b)
+    {
+        return (double) a / b;
+    }
+    public double CalcDiv (int a, int b)
+    {
+        return (double) a / b;
+    }
+    public double CalcMulti (double a, double b)
+    {
+        return a * b;
+    }
+    public long CalcMulti (long a, long b)
+    {
+        return a * b;
+    }
+    public int CalcMulti (int a, int b)
+    {
+        return a * b;
+    }
+    public double CalcSubstr (double a, double b)
+    {
+        return a - b;
+    }
+    public long CalcSubstr (long a, long b)
+    {
+        return a - b;
+    }
+    public int CalcSubstr (int a, int b)
+    {
+        return a - b;
+    }
+}
